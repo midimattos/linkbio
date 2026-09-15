4d896af35302e9282fabfc8d3aab1494c6a2161b
Midian de Mattos Oliveira <midianmattos@hotmail.com>
2026-09-15 13:40:46 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,9 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(req: NextRequest) {
   const cookie = req.cookies.get("admin_session")?.value;
   if (cookie !== process.env.ADMIN_PASSWORD) {
-    return NextResponse.redirect(new URL("/admin", req.url));
+    const url = req.nextUrl.clone();
+    url.pathname = "/admin";
+    return NextResponse.redirect(url);
   }
   return NextResponse.next();
 }
